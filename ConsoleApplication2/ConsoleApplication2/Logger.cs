cc38e4c09f5393d7239842d263410880e0407d6e
AirN05 <nastyafilip@mail.ru>
2018-02-20 23:48:09 +0500

    add empty project

---
+++ ConsoleApplication2/ConsoleApplication2/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Logger : ILogger
+    {
+   
+        private static Logger instance;
+
+        public static Logger GetLogger()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DryIoc;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Container();
+            c.Register<IDatabase>();
+           // IDatabase db = c.Resolve<IDatabase>;
+        }
+    }
+}
